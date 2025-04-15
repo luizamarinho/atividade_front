9b754d9e38cc2ad8bdfbf91cda8b7dcde09034b8
Luiza Marinho <luiza.calheirosm@gmail.com>
2025-04-15 18:08:47 -0300

    Atv Front

---
+++ script.js
@@ -0,0 +1,15 @@
+let valor_ref = document.querySelector('.valor');
+let quantidade_ref = document.querySelector('.quantidade');
+let resultado_ref = document.querySelector('.resultado');
+
+function adicionar() {
+  let valor = parseFloat(valor_ref.value);
+  let quantidade = parseFloat(quantidade_ref.value);
+
+  if (!isNaN(valor)) {
+    let resultado = valor * quantidade;
+    console.log(resultado);
+    resultado_ref.innerHTML = 'Total a pagar: R$' + resultado.toString();
+    resultado_ref.classList.remove('sumir');
+  }
+}
